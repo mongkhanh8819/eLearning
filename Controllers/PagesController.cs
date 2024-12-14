e79ca033afe26df7d3b68c5fc9aeef7455e7d7dc
Khanh <mongkhanh8819@gmail.com>
2024-12-14 20:26:35 +0700

    new project

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Learning.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult OurTeam()
+        {
+            return View();
+        }
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="e_Learning.MvcApplication" Language="C#" %>
